8fef2445f73d114ee16d19ce5dcb48de01ebfad0
Temitope <adesiyantope2014@gmail.com>
2022-05-02 15:44:01 +0100

    e

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,11 +1,15 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { Form } from "react-bootstrap";
+import { FormControl } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 const Product = () => {
   const [product, setProduct] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
- 
+  const [searchField, setSearchField] = useState("");
+
   const navigate = useNavigate();
 
   const fetchProduct = async () => {
@@ -17,14 +21,12 @@ const Product = () => {
         (result) => {
           setIsLoading(false);
           setProduct(result);
-          
         },
         // Note: it's important to handle errors here
         // instead of a catch() block so that we don't swallow
         // exceptions from actual bugs in components.
         (error) => {
           setIsLoading(false);
-          
         }
       );
   };
@@ -33,49 +35,73 @@ const Product = () => {
     fetchProduct();
   }, []);
 
+  const handleChange = (e) => {
+    setSearchField(e.target.value);
+  };
+
+
   if (isLoading) {
     return "Loading....";
   } else {
     return (
-      <div className="product-grid">
-        {product.data.map((item) => {
-          const { id, media_url, thumbnail_url } = item;
-          console.log(media_url)
-          return (
-            <div
-              key={id}
-              onClick={() => navigate(`products/${id}`)}
-              className="product-container"
-            >
-              <div className="product">
-                <a href="/">
-                  <img src={media_url || thumbnail_url} alt="product" />
-                </a>
-              </div>
+      <div>
+        <Form className="d-flex">
+          <FormControl
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+            value={searchField}
+            onChange={handleChange}
+          />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+        <div className="product-grid">
+          {
+            product.data.filter(post => {
+              if (searchField === '') {
+                return post
+              } else if (post.caption.toLowerCase().includes(searchField.toLowerCase())) {
+                return post;
+              }
+            }).map((post,index) => (
+              
+              <div
+                  key={index}
+                  onClick={() => navigate(`products/${post.id}`)}
+                  className="product-container"
+                >
+                  <div className="product">
+                    <a href="/">
+                      <img src={post.media_url || post.thumbnail_url} alt="product" />
+                    </a>
+                  </div>
 
-              <div className="overlay">
-                <div className="overlay-container">
-                  <div className="overlay-img">
-                    <svg
-                      width="30"
-                      height="30"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <g fill="none" fillRule="evenodd">
-                        <path d="M1 0h22a1 1 0 011 1v22a1 1 0 01-1 1H1a1 1 0 01-1-1V1a1 1 0 011-1z"></path>
-                        <path
-                          d="M15.333 8.667v10h-10v-10h10zm3.334-3.334v10H17V7H8.667V5.333h10z"
-                          fill="#FFF"
-                        ></path>
-                      </g>
-                    </svg>
+                  <div className="overlay">
+                    <div className="overlay-container">
+                      <div className="overlay-img">
+                        <svg
+                          width="30"
+                          height="30"
+                          viewBox="0 0 24 24"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <g fill="none" fillRule="evenodd">
+                            <path d="M1 0h22a1 1 0 011 1v22a1 1 0 01-1 1H1a1 1 0 01-1-1V1a1 1 0 011-1z"></path>
+                            <path
+                              d="M15.333 8.667v10h-10v-10h10zm3.334-3.334v10H17V7H8.667V5.333h10z"
+                              fill="#FFF"
+                            ></path>
+                          </g>
+                        </svg>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          );
-        })}
+            ))
+          
+          }
+        </div>
       </div>
     );
   }

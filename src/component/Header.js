60bd180cb0e20da900787848daae150d6446b2ef
Temitope <adesiyantope2014@gmail.com>
2022-05-02 15:54:59 +0100

    search

--- src/component/Header.js
+++ src/component/Header.js
@@ -20,15 +20,7 @@ const Header = () => {
 
               
             </Nav>
-            <Form className="d-flex">
-              <FormControl
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
+            
           </Navbar.Collapse>
         </Container>
       </Navbar>
